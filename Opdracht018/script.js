1311f25615dbec1ce3ab1d630454b2b9474399c6
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-15 12:51:27 +0200

    Add files via upload

---
+++ Opdracht018/script.js
@@ -0,0 +1,29 @@
+//Funtion expression
+//const addExp = function (number1, number2) {
+//    var sumA = number1 * number1;
+//    var sumB = number2 * number2;
+//    return sumA*sumB;
+// };
+
+//const result = addExp(3,5);
+//console.log(result);
+
+//Function declaration
+//function addDec (number1, number2) {
+//    var sumA = number1*number1;
+//    var sumB = number2*number2;
+//    return sumA*sumB;
+//}
+
+//const result = addDec(3,5);
+//console.log(result);
+
+//Arrow function
+const addArr = (sumA + sumB) => {
+    var sumA = number1*number1;
+    var sumB = number2*number2;
+    return addArr*addArr;
+};
+
+const result = addArr(1,2);
+console.log(result);
